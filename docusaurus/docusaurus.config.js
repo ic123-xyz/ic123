04fc241bdce41c92320b6d61e69250877149b649
Herbert Yang <hy.mymail@gmail.com>
2023-07-12 11:18:46 +0800

    Replace default edit URL of facebook with ic123.xyz

--- docusaurus/docusaurus.config.js
+++ docusaurus/docusaurus.config.js
@@ -42,14 +42,14 @@ const config = {
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
           editUrl:
-            'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
+            'https://github.com/ic123-xyz/ic123/tree/main/docusaurus',
         },
         blog: {
           showReadingTime: true,
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
           editUrl:
-            'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
+            'https://github.com/ic123-xyz/ic123/tree/main/docusaurus',
         },
         theme: {
           customCss: require.resolve('./src/css/custom.css'),
