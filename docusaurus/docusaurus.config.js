16ed883141e138b25b31de1e181e89c6bf0ad308
Herbert Yang <hy.mymail@gmail.com>
2023-07-12 11:07:05 +0800

    Update production URL for the site

--- docusaurus/docusaurus.config.js
+++ docusaurus/docusaurus.config.js
@@ -11,7 +11,7 @@ const config = {
   favicon: 'img/favicon.ico',
 
   // Set the production url of your site here
-  url: 'https://bp6lc-ziaaa-aaaag-abqyq-cai.icp0.io/',
+  url: 'https://ic123.xyz',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
   baseUrl: '/',
