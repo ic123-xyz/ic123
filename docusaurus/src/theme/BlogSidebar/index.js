afa6a97961029e3bb617986a9d1057786788a611
cylim <cylim226@gmail.com>
2023-12-26 11:56:48 +0800

    refactor: move categories to it own file

--- docusaurus/src/theme/BlogSidebar/index.js
+++ docusaurus/src/theme/BlogSidebar/index.js
@@ -2,17 +2,7 @@ import React from 'react';
 import {useWindowSize} from '@docusaurus/theme-common';
 import BlogSidebarDesktop from '@theme/BlogSidebar/Desktop';
 import BlogSidebarMobile from '@theme/BlogSidebar/Mobile';
-
-const categories = [
-  {
-    title: 'Hong Kong',
-    permalink: '/blog/tags/hong-kong',
-  }, {
-    title: 'NNS',
-    permalink: '/blog/tags/nns',
-  }
-]
-
+import { categories } from './categories';
 export default function BlogSidebar({sidebar}) {
   const windowSize = useWindowSize();
   if (!sidebar?.items.length) {
